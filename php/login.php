99865ae6f4cd44f540d14816c343413ef167288b
Godblevzy <leonobrestati@gmail.com>
2018-11-22 20:34:08 -0200

    php

---
+++ php/login.php
@@ -0,0 +1,18 @@
+<?php
+	$error = false;
+	if(isset($_POST["login"])){
+		$email = $_POST["email"];
+		$password = $_POST["password"];
+		$sucesso = FALSE;
+		$xml = simplexml_load_file("../xml/usersfolder/usersfile.xml");
+
+		foreach ($xml->user as $user) {
+			if ($password == $user->password && $email == $user->email){
+				$sucesso == TRUE;
+				session_start();
+				$_SESSION["email"] = $email;
+				header("Location: ../pag/pagstart.html");
+			}
+		}
+	} 
+?>
